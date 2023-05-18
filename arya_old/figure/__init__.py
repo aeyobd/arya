9e29292cc72334f4dcc5b4742f231e89ddd82a77
Daniel Boyea <aeyobd@gmail.com>
2023-05-18 16:09:58 -0400

    packaged

--- arya_old/figure/__init__.py
+++
@@ -1,9 +0,0 @@
-from .figure import Figure, save_at
-from .subplot import Subplot
-from .legend import Legend
-from .joinplot import JoinPlot
-from .coord import Length
-
-
-from .subfigure import SubFigure
-from .layout import LayoutManager
--- arya_old/plotting/__init__.py
+++
@@ -1,5 +0,0 @@
-from .scatter import Scatter
-from .data import PlotData
-from .colorbar import Colorbar
-from .line import Line
-
